a808772c04bbf5521dca93172e6b59602a2ec8be
= <=>
2018-03-21 15:15:03 -0600

    Fixing tests and ci hopefully.

---
+++ test/test.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const mocha = require('mocha'),
+    chai = require('chai'),
+    expect = chai.expect;
+
+describe('test suite', function() {
+    describe('test method', function() {
+        it('should parse scan', function() {
+            return true;
+        });
+    });
+});
