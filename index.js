03dd993ff431c29c85d0c2eb4c3f2a56153a47f8
= <=>
2018-03-22 07:39:06 -0600

    Getting sandbox started

--- index.js
+++ index.js
@@ -2,10 +2,15 @@
 
 const requests = require('request-promise-native'),
     endpoints = require('./config/endpoints.json'),
-    applications = require('./lib/applications');
+    applications = require('./lib/applications'),
+    sandboxes = require('./lib/sandboxes');
 
 let apps = applications.getApps();
 apps.then((response) => {
     console.log(response);
 });
 
+let boxes = sandboxes.getSandboxes(294558);
+boxes.then((response) => {
+    console.log(response);
+});
