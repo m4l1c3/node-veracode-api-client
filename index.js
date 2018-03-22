3165fc126eabf36f710fe6be427c0e0bbe09e63c
= <=>
2018-03-22 09:07:21 -0600

    Plumbing getting buildlist and getting buildinfo

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const requests = require('request-promise-native'),
     endpoints = require('./config/endpoints.json'),
     applications = require('./lib/applications'),
     _ = require('underscore'),
-    sandboxes = require('./lib/sandboxes');
+    sandboxes = require('./lib/sandboxes'),
+    builds = require('./lib/builds');
 
 let apps = applications.getApps();
 apps.then((response) => {
@@ -12,7 +13,18 @@ apps.then((response) => {
     _.forEach(response, (item) => {
         let boxes = sandboxes.getSandboxes(item.app_id);
         boxes.then((sandbox) => {
-            console.log(sandbox);
+            _.forEach(sandbox, (box) => {
+                let build = builds.getBuilds(item.app_id, box.sandbox_id);
+                build.then((b) => {
+                    console.log(build);
+                    _.forEach(b, (bb) => {
+                        let bui = builds.getBuild(item.app_id, bb.build_id);
+                        bui.then((objBuild) => {
+                            console.log(objBuild);
+                        });
+                    });
+                });
+            });
         });
     });
 });
