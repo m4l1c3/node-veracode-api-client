d048e8b4189c3819b1ef7991ae61a6f977b55725
= <=>
2018-03-22 07:42:40 -0600

    Adding chaining for apps and sandboxes.

--- index.js
+++ index.js
@@ -3,14 +3,17 @@
 const requests = require('request-promise-native'),
     endpoints = require('./config/endpoints.json'),
     applications = require('./lib/applications'),
+    _ = require('underscore'),
     sandboxes = require('./lib/sandboxes');
 
 let apps = applications.getApps();
 apps.then((response) => {
     console.log(response);
+    _.forEach(response, (item) => {
+        let boxes = sandboxes.getSandboxes(item.app_id);
+        boxes.then((sandbox) => {
+            console.log(sandbox);
+        });
+    });
 });
 
-let boxes = sandboxes.getSandboxes(294558);
-boxes.then((response) => {
-    console.log(response);
-});
