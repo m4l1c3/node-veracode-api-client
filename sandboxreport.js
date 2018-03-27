d28850c0aec79a4e236c4ab0db2e88e773df679b
= <=>
2018-03-27 09:00:16 -0600

    More work on sandbox reporting.:

--- sandboxreport.js
+++ sandboxreport.js
@@ -3,13 +3,17 @@
 const requests = require('request-promise-native'),
     endpoints = require('./config/endpoints.json'),
     applications = require('./lib/applications'),
-    _ = require('underscore'),
     sandboxes = require('./lib/sandboxes'),
     builds = require('./lib/builds'),
     format = require('string-format'),
+    logger = require('./lib/logger'),
+    excel = require('exceljs'),
+    moment = require('moment'),
+    _ = require('underscore'),
     asynchronous = require('async');
 
-//format.extend(String.prototype)
+format.extend(String.prototype)
+
 let application_list = [];
 
 async function processArray(array, processingFunc, objArgs) {
@@ -96,15 +100,70 @@ async function getSandboxes() {
     await getAppSandboxes(application_list);
 }
 
+function createWorkbook() {
+    let workbook = new excel.Workbook();
+    workbook.creator('it-veracodeadmins@scentsy.com');
+    workbook.created = new Date();
+}
+
+function createAggregateWorksheet(workbook, data) {
+    let worksheet = workbook.addWorksheet('Aggregate');
+    worksheet.columns = [
+        {header: 'UserName', key: ''},
+        {header: 'TotalBuilds', key: ''}
+    ];
+}
+
+function createScansWorksheet(workbook, data) {
+    let worksheet = workbook.addWorksheet('Scans');
+    worksheet.columns = [
+        {header: 'User', key: ''},
+        {header: 'Application', key: ''},
+        {header: 'BuildDate', key: ''},
+        {header: 'BuildID', key: ''}
+    ]
+}
+
+function createSpreadsheet(data) {
+    let workbook = createWorkbook();
+    createAggregateWorksheet(workbook, data);
+    createScansWorksheet(workbook, data);
+}
+
+function hasValidBuilds(builds) {
+    var isValid = false;
+    _.any(builds, (build) => {
+        let date = new Date(build.date.substring(0, build.date.lastIndexOf(' ')));
+        if(moment(date).isAfter(moment().subtract(7, 'days'))) {
+            isValid = true;
+        }
+    });
+    return isValid;
+}
+
 async function main() {
-    var start = new Date();
+    logger.info('Beginning sandbox report generation')
+    let start = new Date();
     await getApps();
     await getSandboxes();
     await getSandboxBuilds();
     await getAllBuilds();
-    console.log(application_list);
-    var end = new Date() - start;
-    console.log('Execution time: %dms', end);
+    let apps = _.reject(application_list, (application) => {
+        return _.any(application.sandboxes, (sandbox) => {
+            return !hasValidBuilds(sandbox.builds);
+            // return _.any(sandbox.builds, (build) => {
+            //     let date = new Date(build.date.substring(0, build.date.lastIndexOf(' ')));
+                // return sandbox.builds.length < 1 && !moment(date).isAfter(moment().subtract(7, 'days'));
+            // });
+        });
+        logger.info(application);
+    });
+    if(apps.length) {
+        let spreadsheet = createSpreadsheet(apps);
+    }
+    
+    let end = (new Date() - start) / 1000;
+    logger.info('Total Execution time: {} seconds'.format(end));
 }
 
 main();
